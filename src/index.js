0601110f7f1a218190e17260668558369e2a5228
Na Lin <lna483018@gmail.com>
2022-12-31 17:15:21 -0500

    complete header, with nav bar and mobile menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+// Hamburger menu
+const btn__hamburger = document.getElementById("menu-btn");
+const mobile_menu = document.getElementById("menu");
+
+btn__hamburger.addEventListener("click", navToggle);
+
+function navToggle() {
+  btn__hamburger.classList.toggle("open");
+  mobile_menu.classList.toggle("flex");
+  mobile_menu.classList.toggle("hidden");
+}
