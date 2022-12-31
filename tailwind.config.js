d11dfd891eb597f9f42d808f1255d59de38249b8
Na Lin <lna483018@gmail.com>
2022-12-31 15:45:49 -0500

    add color and font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        softBlue: "hsl(231, 69%, 60%)",
+        softRed: "hsl(0, 94%, 66%)",
+        grayishBlue: "hsl(229, 8%, 60%)",
+        veryDarkBlue: "hsl(229, 31%, 21%)",
+      },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+        league: ["League Spartan", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
