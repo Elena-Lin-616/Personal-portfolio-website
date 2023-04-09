af4fdeaa8bc6a69685a6a54d6e554e42d3d64ad6
Na Lin <lna483018@gmail.com>
2023-04-09 16:45:20 -0400

    add js/ts script to toggle mode icon

--- js/script.js
+++ js/script.js
@@ -7,3 +7,41 @@ function navToggle() {
     mobile_menu.classList.toggle("flex");
     mobile_menu.classList.toggle("hidden");
 }
+// Dark/Light Mode
+var themeToggleBtn = document.getElementById("theme-toggle");
+var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+// show dark/light icon based on setting
+if (localStorage.getItem("color-theme") === "dark" ||
+    (!localStorage.getItem("color-theme") &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    themeToggleLightIcon.classList.remove("hidden");
+}
+else {
+    themeToggleDarkIcon.classList.remove("hidden");
+}
+themeToggleBtn.addEventListener("click", toggleMode);
+function toggleMode() {
+    themeToggleDarkIcon.classList.toggle("hidden");
+    themeToggleLightIcon.classList.toggle("hidden");
+    if (localStorage.getItem("color-theme")) {
+        if (localStorage.getItem("color-theme") === "light") {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+        }
+        else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+        }
+    }
+    else {
+        if (document.documentElement.classList.contains("dark")) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+        }
+        else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+        }
+    }
+}
